af1c0f2884d842de9cdbeff88931f17dc44c5b35
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2020-03-09 20:10:58 +0200

    first commit

---
+++ src/main/java/ruslan/simakov/amazon_analyzer/AmazonAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.amazon_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruslan/simakov/amazon_analyzer/AmazonAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.amazon_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
